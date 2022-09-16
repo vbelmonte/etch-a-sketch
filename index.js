da6843c29a3da03924b8695383221be987d24599
Villy Belmonte <vjbbelmont@gmail.com>
2022-09-16 13:20:38 -0700

    Implement grid line, background, and pen color functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let penColor = document.getElementById("pen-color-picker");
+let bgColor = document.getElementById("bg-color-picker");
+let glColor = document.getElementById("gl-color-picker");
+let penColorValue = penColor.value;
+let bgColorValue = bgColor.value;
+let glColorValue = glColor.value;
+
 function createDiv(gridNumber) {
     let div = document.createElement("div");
     div.style.background = "white";
@@ -18,6 +25,51 @@ function createGrid(gridNumber) {
     }
     createGridBorder(gridNumber);
     document.getElementById("sketch-area").appendChild(toAdd);
+}
+
+function getGridSize() {
+
+}
+
+function getPenColor() {
+    penColor.addEventListener("input", function() {
+        penColorValue = penColor.value;
+    });
+    return penColorValue;
+}
+
+function assignPenColor(value) {
+    penColor.value = value;
+}
+
+function getBackgroundColor() {
+    bgColor.addEventListener("input", function() {
+        bgColorValue = bgColor.value;
+    });
+    return bgColorValue;
+}
+
+function assignBackgroundColor(value) {
+    bgColor.value = value;
+}
+
+function getGridLineColor() {
+    glColor.addEventListener("input", function() {
+        glColorValue = glColor.value;
+    });
+    return glColorValue;
+}
+
+function assignGridLineColor(value) {
+    glColor.value = value;
+}
+
+function toggleGridLines() {
+
+}
+
+function clearCanvas() {
+
 }
 
 
