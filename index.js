ffd5c9a4da6e514a8d053bbe9fd7ec4ca741657a
Villy Belmonte <vjbbelmont@gmail.com>
2022-09-27 19:23:18 -0700

    Implement darken and lighten effects

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let eraserTool = document.getElementById("eraser");
 let penColor = document.getElementById("pen-color-picker");
 let bgColor = document.getElementById("bg-color-picker");
 let glColor = document.getElementById("gl-color-picker");
+let lightenTool = document.getElementById("lighten");
+let darkenTool = document.getElementById("darken");
 let penColorValue = penColor.value;
 let bgColorValue = bgColor.value;
 let glColorValue = glColor.value;
@@ -145,6 +147,68 @@ function clearCanvas() {
 }
 
 
+function printOutHSL(input) {
+    let color = HexToHSL(RGBToHex(input));
+    
+    return 'hsl(' + color.h + ', ' + color.s + '%, ' + color.l + '%)';
+}
+
+function HexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        
+        h /= 6;
+    }
+
+    s = s*100;
+    s = Math.round(s);
+    l = l*100;
+    l = Math.round(l);
+    h = Math.round(360*h);
+
+    return {h, s, l};
+}
+
+
+function RGBToHex(rgb) {
+    // Choose correct separator
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+  
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+  
+    return "#" + r + g + b;
+}
+
+
 
 /****************************
 *
@@ -195,6 +259,33 @@ function pen() {
     assignCurrentTool("pen");
 }
 
+function lighten() {
+    assignCurrentTool("lighten");
+}
+
+function darken() {
+    assignCurrentTool("darken");
+}
+
+function lightenEffect(input) {
+    let color = HexToHSL(input);
+    let newColor = color.l + 1;
+    console.log(newColor);
+
+    return 'hsl(' + color.h + ', ' + color.s + '%, ' + newColor + '%)';
+}
+
+function darkenEffect(input) {
+    let color = HexToHSL(input);
+    let newColor = color.l - 1;
+    console.log(newColor);
+
+    return 'hsl(' + color.h + ', ' + color.s + '%, ' + newColor + '%)';
+}
+
+
+
+
 
 function addDivEventListener(theDiv) {
     theDiv.addEventListener(/*"mousemove"*/"pointermove", function() {
@@ -205,6 +296,12 @@ function addDivEventListener(theDiv) {
             }
             else if (getCurrentTool() === "eraser") {
                 theDiv.style.backgroundColor = "#FFFFFF";
+            }
+            else if (getCurrentTool() === "lighten") {
+                theDiv.style.background = lightenEffect(RGBToHex(theDiv.style.backgroundColor));
+            }
+            else if (getCurrentTool() === "darken") {
+                theDiv.style.background = darkenEffect(RGBToHex(theDiv.style.backgroundColor));
             }
         }
     });
@@ -231,6 +328,12 @@ function addDivClickListener(theDiv) {
         }
         else if (getCurrentTool() === "eraser") {
             theDiv.style.backgroundColor = "#FFFFFF";
+        }
+        else if(getCurrentTool() === "lighten") {
+            theDiv.style.background = lightenEffect(RGBToHex(theDiv.style.backgroundColor));
+        }
+        else if (getCurrentTool() === "darken") {
+            theDiv.style.background = darkenEffect(RGBToHex(theDiv.style.backgroundColor));
         }
     });
 }
@@ -260,6 +363,8 @@ toggleGrid.addEventListener("click", function() {
 penTool.addEventListener("click", pen);
 eraserTool.addEventListener("click", erase);
 clearTool.addEventListener("click", clearCanvas);
+lightenTool.addEventListener("click", lighten);
+darkenTool.addEventListener("click", darken);
 
 pressedButton();
 createGrid(10);
